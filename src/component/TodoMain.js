dd67b271f5c820aa8af2e6523c19f0999e8684c1
Yoyo <youyou@yonyou.com>
2017-11-23 09:02:28 +0800

    eslint

--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -1,17 +1,24 @@
-import React from "react";
-import TodoItem from "./TodoItem"
+import React from 'react';
+import TodoItem from './TodoItem';
 
-class TodoMain extends React.Component{
-    // 遍历显示任务，转发props
-    render(){
-        return (
-            <ul className="todo-list">
-                {this.props.todos.map((todo, index) => {
-                    return <TodoItem key={index} {...todo} index={index} {...this.props}/>
-                })}
-            </ul>
-        )
-    }
+class TodoMain extends React.Component {
+  // 遍历显示任务，转发props
+  render() {
+    return (
+      <ul className="todo-list">
+        {this.props.todos.map((todo, index) => {
+          return (
+            <TodoItem
+              key={index}
+              {...todo}
+              index={index}
+              {...this.props}
+            />
+        );
+        })}
+      </ul>
+    );
+  }
 }
 
-export default TodoMain
+export default TodoMain;
